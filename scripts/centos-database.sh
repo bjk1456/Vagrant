c63971c713cc7ec1423670bcd01fef19271eb59a
Ben K <NixonsGhost@gmail.com>
2016-01-25 11:48:07 -0500

    Removed extraneous text

--- scripts/centos-database.sh
+++ scripts/centos-database.sh
@@ -8,10 +8,3 @@ chkconfig mysql on
 service mysqld start
 
 mysql -u root -e "SHOW DATABASES";
-
-#  Download Starter Content
-cd /vagrant
-sudo -u vagrant wget -q https://raw.githubusercontent.com/bjk1456/Vagrant/master/files/index.html
-sudo -u vagrant wget -q https://raw.githubusercontent.com/bjk1456/Vagrant/master/files/info.php
-
-service httpd restart
