5c5a7fe045012725a48a9911b2b625d1ffd1f891
Ben Kelly <Benjamin.Kelly@WindsorLakeComputing.com>
2016-02-28 18:33:46 -0500

    Add support for Fletcher Nichol's Bash Profile Chef Cookbook

---
+++ chef/supermarket/bashrc-master/recipes/user.rb
@@ -0,0 +1,20 @@
+user_home = value_for_platform_family(
+  "mac_os_x" => "/Users/vagrant",
+  "default" => "/home/vagrant"
+)
+
+user "vagrant" do
+  home user_home
+  manage_home true
+  shell "/bin/bash"
+  gid "admin" if platform_family?("mac_os_x") # on mac, no group, no create :(
+end
+
+# Looks like the user doesn't own their own home directory on
+# mac_os_x platforms, so let's fix that up
+directory user_home do
+  owner "vagrant"
+  only_if { platform_family?("mac_os_x") }
+end
+
+bashrc "vagrant"
--- chef/supermarket/virtualenvwrapper/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# 0.1.1
-
-* Fixed metadata typo for supported platforms.
-
-# 0.1.0
-
-* Initial release of virtualenvwrapper
